28cf7eafb6213a8cf06aa1a4b8c83b8c1e11effe
cheilamelo <cheila.melo@routeasy.com.br>
2023-12-01 11:40:05 -0300

    adjust section Contact Us of the desktop and mobile Contact page

--- components/layout/ContactUs/component/Card.tsx
+++ components/layout/ContactUs/component/Card.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export const Card = ({ src, alt, title, subtitle }: any) => {
   return (
-    <div className="border border-purplescale-200 rounded-lg px-4 py-10 w-[347px] flex items-center mb-8 md:mb-0">
+    <div className="border border-purplescale-200 rounded-lg px-4 py-10 lg:w-[347px] 3xl:w-[505px] flex items-center mb-4 lg:mb-0">
       <div className="rounded-full w-12 h-12 relative bg-purplescale-300 flex items-center justify-center">
         <Image src={src} alt={alt} width={24} height={24} />
       </div>
--- components/layout/ContactUs/index.tsx
+++ components/layout/ContactUs/index.tsx
@@ -7,7 +7,7 @@ export default function ContactUs() {
   return (
     <Section className="mb-8">
       <Text className="text-base font-medium mb-4">Entre em contato</Text>
-      <div className="flex justify-between">
+      <div className="lg:flex justify-between">
         {list_card_contact_us.map((item, index) => (
           <Card
             key={index}
